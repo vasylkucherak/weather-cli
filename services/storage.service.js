6813187b0fa0885eaf489d321cb75a64bb0db995
Vasyl Kucherak <vasil.kucherak@gmail.com>
2022-06-02 00:42:44 +0300

    v.0.2.0(added storage.service file)

---
+++ services/storage.service.js
@@ -0,0 +1,16 @@
+import { homedir } from 'os'; //? значення домашньої папки користувача PC
+import { join, basename, dirname, extname, relative, isAbsolute, resolve, sep } from 'path'; //? з 2-х строк робить шлях
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const saveKeyValue = (key, value) => {
+    console.log(basename(filePath));
+    console.log(dirname(filePath));
+    console.log(extname(filePath));
+    console.log(relative(filePath, dirname(filePath)));
+    console.log(isAbsolute(filePath));
+    console.log(resolve('..'));
+    console.log(sep);
+};
+
+export { saveKeyValue };
--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
 import { printHelp } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
@@ -11,7 +12,7 @@ const initCLI = () => {
 		//? зберегти місто
 	}
 	if (args.t) {
-		//? зберегти токен
+		saveKeyValue('token', args.t);
 	}
 	//? вивести погоду
 };
